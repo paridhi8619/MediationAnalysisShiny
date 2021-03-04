1dd732c4ac8718f75e0b294961bf3be04119a985
Paridhi <paridhi.jain@pwc.com>
2021-03-04 20:52:32 +0530

    Added simulation

--- dashboard.r
+++ dashboard.r
@@ -10,11 +10,8 @@ library(shinyBS)
 library(ggplot2)
 library(mice)
 library(shinydashboardPlus)
-# install.packages("devtools")
-# library(devtools)
-# install_github("easyGgplot2", "kassambara")
-# install_github("kassambara/easyGgplot2")
 library(mediation)
+library(simstudy)
 # library(plyr)
 options(digits=5)
 fileData <- read.csv("C:/Users/paridhij747/Documents/Lilly/LillyShiny/data/mediation_data.csv")
@@ -99,7 +96,11 @@ ui <- dashboardPage(skin="red",
                             #                 sliderInput("rednesssd", "Mean: redness", round(min(fileData$redness), 2), round(max(fileData$redness), 2), value = ""))),
                             fluidRow(column(4, verbatimTextOutput("itchMedPerc"))),
                             fluidRow(column(4,
-                                            plotOutput("itchPlot")))
+                                            plotOutput("itchPlot")),
+                                     column(4,
+                                            plotOutput("BSAPlot")),
+                                     column(4,
+                                            plotOutput("rednessPlot")))
                             
                             
                             )
@@ -167,8 +168,27 @@ server <- function(input, output) {
   # seed <- reactive({
   #   input$seed
   # })
+  simDataGen <- reactive({
+    groupMeans <- aggregate(fileData[, c(2,4,6,8)], list(fileData$TRT), mean)
+    cor <- cor(fileData[,c(2,4,6, 8)])
+    sd <- aggregate(fileData[, c(2,4,6,8)], list(fileData$TRT), sd)
+    num <- as.numeric(input$count)
+    num <- num * 2
+    print(num)
+    simData <- genCorData(num, mu = c(groupMeans$itch[1], groupMeans$BSA[1], groupMeans$redness[1], groupMeans$DLQI[1]), 
+                          sigma = c(sd$itch[1], sd$BSA[1], sd$redness[1], sd$DLQI[1]), corMatrix = cor)
+    simData$V2[simData$V2 < min(fileData$BSA)] <-  min(fileData$BSA)
+    simData$V2[simData$V2 > max(fileData$BSA)] <-  max(fileData$BSA)
+    
+    simData$V3[simData$V3 < min(fileData$redness)] <-  min(fileData$redness)
+    simData$V3[simData$V3 > max(fileData$redness)] <-  max(fileData$redness)
+    simData <- as.data.frame(simData[, c(3,4)])
+    names(simData) <- c("BSA", "redness")
+    simData
+  })
+ 
   
-  itchReactive <- reactive({
+  medDataGen <- reactive({
     muPlacebo <- 5.925
     muTrt <- 5.925 - 1.437 
     
@@ -181,13 +201,60 @@ server <- function(input, output) {
     finalData <- rbind(dataPlacebo, dataTrt)
     random2=runif(nrow(finalData),min=min(finalData$itch),max=max(finalData$itch))
     finalData$DLQI=finalData$itch*input$medItch+random2*0.65
-    b <- lm(formula = DLQI ~ as.factor(TRT), data = finalData)
-    c <- lm(formula = DLQI ~ as.factor(TRT) + itch, data=finalData)
+    finalData
+  })
+  
+  itchReactive <- reactive({
+    # muPlacebo <- 5.925
+    # muTrt <- 5.925 - 1.437 
+    # 
+    # sePlacebo <- 0.232
+    # seTrt <- sqrt(0.232^2/120 + 0.328^2/120)
+    # set.seed(input$seed)
+    # count <- input$count
+    # dataPlacebo <- data.frame("itch" = rnorm(count, muPlacebo, sePlacebo), "TRT" = "Placebo")
+    # dataTrt <- data.frame("itch" = rnorm(count, muTrt, 0.328), "TRT" = "Rx")
+    # finalData <- rbind(dataPlacebo, dataTrt)
+    # random2=runif(nrow(finalData),min=min(finalData$itch),max=max(finalData$itch))
+    # finalData$DLQI=finalData$itch*input$medItch+random2*0.65
+    # finalData$BSA = 
+    medDataGen <- medDataGen()
+    b <- lm(formula = DLQI ~ as.factor(TRT), data = medDataGen)
+    c <- lm(formula = DLQI ~ as.factor(TRT) + itch, data=medDataGen)
     itchMed <- mediate(b, c, sims=100, treat="as.factor(TRT)", mediator="itch")
     itchMed
   })
+  mergeData <- reactive({
+    df1 <- simDataGen()
+    df2 <- medDataGen()
+    df <- cbind(df1, df2)
+    df
+  })
+  BSAReactive <- reactive({
+    df <- mergeData()
+    print(head(df))
+    b <- lm(formula = DLQI ~ as.factor(TRT), data = df)
+    c <- lm(formula = DLQI ~ as.factor(TRT) + BSA, data=df)
+    BSAMed <- mediate(b, c, sims=100, treat="as.factor(TRT)", mediator="BSA")
+    BSAMed
+    
+  })
+  rednessReactive <- reactive({
+    # browser()
+    df <- mergeData()
+    b <- lm(formula = DLQI ~ as.factor(TRT), data = df)
+    c <- lm(formula = DLQI ~ as.factor(TRT) + redness, data=df)
+    rednessMed <- mediate(b, c, sims=100, treat="as.factor(TRT)", mediator="redness")
+    rednessMed
+  })
   output$itchPlot <- renderPlot({
       plot(itchReactive())
+  })
+  output$BSAPlot <- renderPlot({
+    plot(BSAReactive())
+  })
+  output$rednessPlot <- renderPlot({
+    plot(rednessReactive())
   })
   output$itchMedPerc <- renderText({
     paste("Proportions mediated = ", round(itchReactive()$n0, 2), "%")
